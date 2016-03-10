32911821dc5248edd09a4b81300de8c300f45ea4
KyleRuan <kobe8329@gmail.com>
2016-03-11 00:53:03 +0800

    添加注册组件

--- Ours/AppDelegate.swift
+++ Ours/AppDelegate.swift
@@ -7,6 +7,11 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
+import DigitsKit
+import Appsee
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        Fabric.with([Crashlytics.self,Appsee.self,Digits.self])
         // Override point for customization after application launch.
         return true
     }
