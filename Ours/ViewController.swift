21c487675f63b66698d9a041656d24fa1f254710
KyleRuan <kobe8329@gmail.com>
2016-03-10 17:11:40 +0800

    first init
    begin Ours Project

---
+++ Ours/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ours
+//
+//  Created by Jason on 16/3/10.
+//  Copyright © 2016年 com.kyleruan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
